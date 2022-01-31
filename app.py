b7fb3a1ac9916a9547dbabe0d78be453a6a1cbd9
evagpm <evagpm98@gmail.com>
2022-01-31 00:33:12 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,88 +1,87 @@
 '''
-In this example, we are going to look at creating, serving, and deploying a *very simple* webapp. In the following several lecture, we'll see how to add some interesting interactivity to our app. 
-
-# Prerequisites
-
-- You need to have the flask package installed in your PIC16B Anaconda environment. 
-- You need a Heroku account.
-- You need the Heroku command line interface: 
-    - To install, at the command line (for MacOS)
-    brew tap heroku/brew && brew install heroku
-
-- At the command line, run 
-    conda activate PIC16B
-
-# Local Preview
-
 At the command line: 
 conda activate PIC16B
 export FLASK_ENV=development; flask run
 
-# Deployment
-
-*Note*: these notes are written for the version of the app that Phil is using, which is indeed called pic16b-minimal-demo. In order to make your own version, you would need to give the app a different name (because one with this name already exists on the internet now).
-
-Sign up for Heroku, create app called pic16b-minimal-demo
-
-```
-heroku login
-heroku git:remote -a pic16b-minimal-demo
-
-git add *.
-git commit -m'add files for heroku'
-git push heroku
-```
-
-Then, the website is at 
-https://pic16b-minimal-demo.herokuapp.com
-
-    
-# Sources
-
-This set of lecture notes is based in part on previous materials developed by [Erin George](https://www.math.ucla.edu/~egeo/) (UCLA Mathematics) and the tutorial [here](https://stackabuse.com/deploying-a-flask-application-to-heroku/). 
 '''
 
-from flask import Flask, render_template
+from flask import Flask, render_template, g, request
+import sqlite3
+
 app = Flask(__name__)
 
+#Show base page with links to submit and view
 @app.route("/")
 def main():
-    #return "seeing if I can change it!"
-    return render_template("base.html")
+    return render_template("basic.html")
 
-
-'''
-from flask import Flask, render_template, request
-app = Flask(__name__)
-
-@app.route("/")
-# simplest possible approach
-def main():
-    return "hi there!"
-
-# slightly less trivial
-def main():
-     return render_template("main.html")
-
-# A little fancier
-
-# @app.route("/")
-#def main():
-#    return render_template("main_better.html")
-
-# getting basic user data
-@app.route('/ask/', methods=['POST', 'GET'])
-def ask():
-    if request.method == 'GET':
-        return render_template('ask.html')
+#Establish submit page
+@app.route("/submit", methods=['POST', 'GET'])
+def submit():
+    if request.method == 'GET' :
+        return render_template("submit.html")
     else:
-        try:
-            return render_template('ask.html', name=request.form['name'], student=request.form['student'])
-        except:
-            return render_template('ask.html')
+        insert_message(request)
+        return render_template("submit.html", done = True)
 
-# 
-@app.route('/profile/<name>/')
-def hello_name(name):
-    return render_template('profile.html', name=name)
-'''
+#Function retrieves or creates the connection
+def get_message_db():
+    #Checks if message_db exists in g
+    try:
+        g.message_db
+
+    #If it doesn't, it creates it
+    except AttributeError:
+        g.message_db = sqlite3.connect("messages_db.sqlite")
+        cursor = g.message_db.cursor()
+
+        #Also creates a messages table, if needed
+        cmd = """
+            CREATE TABLE IF NOT EXISTS messages (
+            id int,
+            handle varchar(50),
+            message varchar(200))
+            """
+        cursor.execute(cmd)
+
+    #Returns that connection
+    return g.message_db
+
+#Function inserts the message into the table
+def insert_message(request):
+    #Get inputs from form
+    message = request.form['message']
+    handle = request.form['handle']
+
+    #Use function to get cursor
+    cursor = get_message_db().cursor()
+
+    #Count the number of rows in messages
+    cursor.execute("SELECT COUNT(*) FROM messages")
+    nrows = cursor.fetchall()[0][0]
+
+    #Add row for new message
+    cmd = "INSERT INTO messages VALUES ("
+    cmd += str(nrows + 1) + ", \"" + handle + "\", \"" + message + "\")"
+    cursor.execute(cmd)
+    get_message_db().commit() #Commit changes so they save
+    get_message_db().close() #Always close connection when done
+
+#Establish view page
+@app.route("/view")
+def view():
+    rows = random_messages(6)
+    return render_template('view.html', rows = rows)
+
+#Function returns a list of rows of messages from the table
+def random_messages(n):
+    #Use function to get cursor
+    cursor = get_message_db().cursor()
+
+    #Extract n random rows
+    cmd = "SELECT * FROM messages ORDER BY RANDOM() LIMIT "+ str(n) + ";"
+    cursor.execute(cmd)
+    rows = cursor.fetchall()
+    get_message_db().close() #Always close connection when done
+
+    return rows
